1fed1aee035f6b9a131024c1e0a0886d8a21db5e
Parth099 <parthnpatel715@gmail.com>
2023-04-03 13:33:10 -0400

    changed db connector to be class based to make it easier to use

--- db_connection.py
+++ db_connection.py
@@ -11,22 +11,36 @@ load_dotenv("./config/.env")
 
 # use generated secrets to login into database 
 cred = credentials.Certificate("./config/key.json")
-firebase_admin.initialize_app(cred)
 
-# get ref to databse
-db = firestore.client()
+class FirebaseConnection():
+    def __init__(self, username: str) -> None:
+        # init firebase connection via secrets file
+        firebase_admin.initialize_app(cred)
+        # get ref to databse
+        self.db = firestore.client()
+        self.username = username
 
-# test user upload
-# Get a reference to the 'users' collection
-root_patient_coll_name = getenv('ROOT_PATIENT_COLLECTIONS')
+    def __test(self):
+        """test database connection by uploading a default dict to the firebase document under `self.username`
+        """
 
-patients_coll_ref = db.collection(root_patient_coll_name)
+        # test user upload
+        root_patient_coll_name = getenv('ROOT_PATIENT_COLLECTIONS')
 
-new_patients_ref = patients_coll_ref.document('p1')
+        # get collection ref 
+        patients_coll_ref = self.db.collection(root_patient_coll_name)
 
-new_patients_ref.set({
-	'name': 'John Doe',
-    'email': 'john@example.com',
-    'age': 30
-})
+        # use collection to create document ref
+        new_patients_ref = patients_coll_ref.document(self.username)
 
+        # set document
+        new_patients_ref.set({
+            'name': 'John Doe',
+            'email': 'john@example.com',
+            'age': 30
+        })
+
+FBC = FirebaseConnection('TestUser1')
+FBC.__test()
+
+        
