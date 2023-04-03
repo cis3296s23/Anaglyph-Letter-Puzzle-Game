d82e4d883906f4c945bd25bddfa77d08b16d4e4d
Regina Oda <tuj58359@temple.edu>
2023-04-03 11:52:05 -0400

    changed button layout, added a title, and fixed small bugs

---
+++ Menu.py
@@ -0,0 +1,119 @@
+import pygame
+import pygame_gui
+
+pygame.init()
+button_width = 200
+button_height = 50
+info = pygame.display.Info()
+
+screen_width = info.current_w
+screen_height = info.current_h
+screen = pygame.display.set_mode((screen_width, screen_height))
+pygame.display.set_caption("Anaglyph Letter Puzzle")
+manager = pygame_gui.UIManager((screen_width, screen_height))
+middle = (screen_width - button_width) / 2
+right = screen_width - button_width - 10
+top = 10
+font = pygame.font.SysFont(None, 100)
+
+
+def menu_page():
+    global manager
+    x = middle
+    y = 200
+    manager = pygame_gui.UIManager((screen_width, screen_height))
+    title_label = pygame_gui.elements.UILabel(relative_rect=pygame.Rect((middle, 100), (button_width, button_height)), text="Anaglyph Letter Puzzle", manager=manager)
+    start_button = pygame_gui.elements.UIButton(relative_rect=pygame.Rect((x, y), (button_width, button_height)), text="Quick Play", manager=manager)
+    mode_button = pygame_gui.elements.UIButton(relative_rect=pygame.Rect((x, y + 100), (button_width, button_height)), text="Mode Select", manager=manager)
+    settings_button = pygame_gui.elements.UIButton(relative_rect=pygame.Rect((x, y + 200), (button_width, button_height)), text="Settings", manager=manager)
+    help_button = pygame_gui.elements.UIButton(relative_rect=pygame.Rect((x, y + 300), (button_width, button_height)), text="Help", manager=manager)
+    login_button = pygame_gui.elements.UIButton(relative_rect=pygame.Rect((x, y + 400), (button_width, button_height)), text="Login", manager=manager)
+    quit_button = pygame_gui.elements.UIButton(relative_rect=pygame.Rect((x, y + 500), (button_width, button_height)), text="Quit", manager=manager)
+
+    running = True
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+            elif event.type == pygame.USEREVENT:
+                if event.user_type == pygame_gui.UI_BUTTON_PRESSED:
+                    if event.ui_element == start_button:
+                        redirect_proof_of_concpt_page()
+                    elif event.ui_element == mode_button:
+                        mode_page()
+                    elif event.ui_element == settings_button:
+                        redirect_proof_of_concpt_page()
+                    elif event.ui_element == help_button:
+                        redirect_proof_of_concpt_page()
+                    elif event.ui_element == login_button:
+                        redirect_proof_of_concpt_page()
+                    elif event.ui_element == quit_button:
+                        running = False
+            manager.process_events(event)
+        manager.update(pygame.time.get_ticks() / 1000.0)
+        screen.fill((0, 0, 0))
+        manager.draw_ui(screen)
+        pygame.display.update()
+    pygame.quit()
+
+def back_to_menu():
+    global manager
+    pygame.display.set_caption("Letter Puzzle")
+    manager = pygame_gui.UIManager((screen_width, screen_height))
+    menu_page()
+
+
+def mode_page():
+    global manager
+    pygame.display.set_caption("Mode Select")
+    manager = pygame_gui.UIManager((screen_width, screen_height))
+    back_button = pygame_gui.elements.UIButton(
+        relative_rect=pygame.Rect(
+            (right, 10),
+            (button_width, button_height)),
+        text="Back",
+        manager=manager)
+
+    running = True
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+            elif event.type == pygame.USEREVENT:
+                if event.user_type == pygame_gui.UI_BUTTON_PRESSED:
+                    if event.ui_element == back_button:
+                        back_to_menu()
+            manager.process_events(event)
+        manager.update(pygame.time.get_ticks() / 1000.0)
+        screen.fill((0, 0, 0))
+        manager.draw_ui(screen)
+        pygame.display.update()
+    pygame.quit()
+
+def redirect_proof_of_concpt_page():
+    global manager
+    manager = pygame_gui.UIManager((screen_width, screen_height))
+    back_button = pygame_gui.elements.UIButton(
+        relative_rect=pygame.Rect(
+            (right, 10),
+            (button_width, button_height)),
+        text="Back",
+        manager=manager)
+
+    running = True
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+            elif event.type == pygame.USEREVENT:
+                if event.user_type == pygame_gui.UI_BUTTON_PRESSED:
+                    if event.ui_element == back_button:
+                        back_to_menu()
+            manager.process_events(event)
+        manager.update(pygame.time.get_ticks() / 1000.0)
+        screen.fill((0, 0, 0))
+        manager.draw_ui(screen)
+        pygame.display.update()
+    pygame.quit()
+
+menu_page()
