56765cf7d1a14a2668fe07506ac2588b489431c5
Parth099 <parthnpatel715@gmail.com>
2023-04-06 15:20:04 -0400

    added save time function

--- db_connection.py
+++ db_connection.py
@@ -1,4 +1,8 @@
-import asyncio # to run async coroutines
+# to run async coroutines
+import asyncio 
+
+# get epoch time
+import time    
 
 # load in public secrets
 from dotenv import load_dotenv
@@ -6,8 +10,12 @@ from os import getenv
 
 # connect to database
 import firebase_admin
-from google.cloud.firestore import Client as Firestore
 from firebase_admin import credentials, firestore_async
+
+# using this to provide type suggestions
+from google.cloud.firestore import Client as Firestore
+from google.cloud.firestore_v1.document import DocumentReference
+
 
 # get public secrets in memory
 load_dotenv("./config/.env")
@@ -15,7 +23,15 @@ load_dotenv("./config/.env")
 # use generated secrets to login into database 
 cred = credentials.Certificate("./config/key.json")
 
+"""
+save color data
+save data function
+"""
+
 class FirebaseConnection():
+    """`FirebaseConnection` provides a set of functions to communicate with the predefined database based on app secrets.
+    It uses `firestore_async` to do so. 
+    """
     def __init__(self, username: str) -> None:
         # init firebase connection via secrets file
         firebase_admin.initialize_app(cred)
@@ -23,18 +39,21 @@ class FirebaseConnection():
         self.db: Firestore = firestore_async.client()
         self.username = username
 
+        self.patients_collection = getenv('ROOT_PATIENT_COLLECTIONS')
+
+        if self.patients_collection is None:
+            raise ValueError("collection value not found, check .env")
+
+
+    def save_time(self, docRef: DocumentReference):
+        return docRef.set({ 'lastUpdated': int(time.time()) }, merge=True)
+        
+
     async def test(self):
         """test database connection by uploading a default dict to the firebase document under `self.username`
         """
-
-        # test user upload
-        root_patient_coll_name = getenv('ROOT_PATIENT_COLLECTIONS')
-
-        if root_patient_coll_name is None:
-            raise ValueError("collection value not found, check .env")
-
         # get collection ref 
-        patients_coll_ref = self.db.collection(root_patient_coll_name)
+        patients_coll_ref = self.db.collection(self.patients_collection)
 
         # use collection to create document ref
         new_patients_ref = patients_coll_ref.document(self.username)
@@ -44,7 +63,9 @@ class FirebaseConnection():
             'name': 'John Doe',
             'email': 'john@example.com',
             'age': 30
-        })
+        }, merge=True)
+
+        await self.save_time(new_patients_ref)
 
         return await result
 
