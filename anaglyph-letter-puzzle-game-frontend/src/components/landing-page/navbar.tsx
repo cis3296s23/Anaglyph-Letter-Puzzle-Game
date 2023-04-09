78e810dc616c95cc1b9118475770df4818cd286f
Parth099 <parthnpatel715@gmail.com>
2023-04-08 22:48:38 -0400

    refactor:navbar

--- anaglyph-letter-puzzle-game-frontend/src/components/landing-page/navbar.tsx
+++ anaglyph-letter-puzzle-game-frontend/src/components/landing-page/navbar.tsx
@@ -1,8 +1,8 @@
+import Link from "next/link";
 import React, { useState } from "react";
 
 import { GiHamburgerMenu } from "react-icons/gi";
 import { IoMdClose } from "react-icons/io";
-import { AiOutlineLock } from "react-icons/ai";
 
 const Navbar = () => {
     const [toggle, setToggle] = useState(false);
@@ -19,6 +19,8 @@ const Navbar = () => {
         </>
     );
 
+    const AuthButton = <Link href={"#"} className="px-8 py-3 rounded-md bg-sp-green text-white font-bold">Sign Up or Log in</Link>;
+
     return (
         <div className="w-full h-[80px] bg-white border-b font-inter sticky">
             <div className="md:max-w-[1480px] max-w-[600px] m-auto w-full h-full flex justify-between items-center md:px-4 px-4">
@@ -27,13 +29,7 @@ const Navbar = () => {
                     <ul className="flex gap-4">{Links}</ul>
                 </div>
 
-                <div className="hidden md:flex">
-                    <button className="flex justify-between items-center bg-transparent px-6 gap-2">
-                        <AiOutlineLock size={25} />
-                        Login
-                    </button>
-                    <button className="px-8 py-3 rounded-md bg-sp-green text-white font-bold">Sign Up</button>
-                </div>
+                <div className="hidden md:flex">{AuthButton}</div>
 
                 <div className="md:hidden" onClick={handleClick}>
                     {toggle ? <IoMdClose size={25} /> : <GiHamburgerMenu size={25} />}
@@ -43,13 +39,7 @@ const Navbar = () => {
             <div className={toggle ? "absolute z-10 p-4  bg-white w-full px-8 md:hidden border-b" : "hidden"}>
                 <ul>
                     {Links}
-                    <div className="flex flex-col my-4 gap-4">
-                        <button className="border border-sp-green flex justify-center items-center bg-transparent px-6 gap-2 py-4">
-                            <AiOutlineLock size={25} />
-                            Login
-                        </button>
-                        <button className="px-8 py-3 rounded-md bg-sp-green text-white font-bold">Sign Up</button>
-                    </div>
+                    <div className="flex flex-col my-4 gap-4">{AuthButton}</div>
                 </ul>
             </div>
         </div>
