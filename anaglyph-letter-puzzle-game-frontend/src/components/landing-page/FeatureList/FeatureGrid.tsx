15762a267a98b4f8bdc1b58394e65ff9d435ff87
Parth099 <parthnpatel715@gmail.com>
2023-04-10 13:53:29 -0400

    fixed colors for feature grid

--- anaglyph-letter-puzzle-game-frontend/src/components/landing-page/FeatureList/FeatureGrid.tsx
+++ anaglyph-letter-puzzle-game-frontend/src/components/landing-page/FeatureList/FeatureGrid.tsx
@@ -6,7 +6,7 @@ import FeatureCard from "./FeatureCard";
 var x = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus pariatur quis nostrum corrupti, eum at";
 export default function Features() {
     return (
-        <section className="font-inter">
+        <section className="font-inter border-b">
             <div className="mx-auto w-[1200px] lg-max:grid-cols-1 lg-max:w-full xl-max:w-[1000px]">
                 <h2 className="text-4xl font-bold pt-10 text-gray-700 xl-max:pl-8">Features</h2>
                 <section className="my-10 grid grid-cols-3 gap-10 justify-items-center xl-max:grid-cols-2 md-max:grid-cols-1">
--- anaglyph-letter-puzzle-game-frontend/src/pages/index.tsx
+++ anaglyph-letter-puzzle-game-frontend/src/pages/index.tsx
@@ -1,4 +1,3 @@
-
 import Features from "@/components/landing-page/FeatureList/FeatureGrid";
 import Hero from "@/components/landing-page/Hero";
 import { Inter } from "next/font/google";
@@ -7,9 +6,9 @@ const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
     return (
-        <>
+        <main className="mb-4">
             <Hero />
             <Features />
-        </>
+        </main>
     );
 }
