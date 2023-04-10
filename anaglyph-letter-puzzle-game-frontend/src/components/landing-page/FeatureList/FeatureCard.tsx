b0dd2f57779ed41f2063b9e575c9c82535130a31
Parth099 <parthnpatel715@gmail.com>
2023-04-10 13:48:19 -0400

    made the icons into a badge

--- anaglyph-letter-puzzle-game-frontend/src/components/landing-page/FeatureList/FeatureCard.tsx
+++ anaglyph-letter-puzzle-game-frontend/src/components/landing-page/FeatureList/FeatureCard.tsx
@@ -8,9 +8,9 @@ interface FeatureCardProps {
 
 export default function FeatureCard(props: FeatureCardProps) {
     return (
-        <div className="shadow-main border-[1px] w-96 h-48 rounded shadow-lg p-4 mid-max:w-[324px] md-max:w-[400px]">
-            <div className="flex gap-4">
-                {props.icon}
+        <div className="shadow-main border-[1px] w-96 h-48 rounded shadow-lg p-4 mid-max:w-[356px] md-max:w-[412px] border-b">
+            <div className="flex">
+                <div className="tranform-icon relative bg-white p-4 rounded-lg shadow-lg border">{props.icon}</div>
                 <h3 className="text-2xl font-semibold">{props.name}</h3>
             </div>
             <p className="pt-3">{props.statement}</p>
