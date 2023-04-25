4e93802c9f4e771df11fb128374f3e804ae19b2a
Parth099 <parthnpatel715@gmail.com>
2023-04-24 21:41:07 -0400

    rm darkmode hints

--- anaglyph-letter-puzzle-game-frontend/src/components/TherapistPage/PatientCard.tsx
+++ anaglyph-letter-puzzle-game-frontend/src/components/TherapistPage/PatientCard.tsx
@@ -30,8 +30,8 @@ export default function PatientCards(props: PatientCardProps) {
 
     return (
         <div className="relative overflow-x-auto shadow-md sm:rounded-lg">
-            <table className="w-full text-sm text-left text-gray-500 dark:text-gray-400">
-                <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+            <table className="w-full text-sm text-left text-gray-500">
+                <thead className="text-xs text-gray-700 uppercase bg-gray-50">
                     <tr>
                         <th scope="col" className="px-6 py-3">
                             Username
@@ -55,8 +55,8 @@ export default function PatientCards(props: PatientCardProps) {
                 </thead>
                 <tbody>
                     {props.users.map((user) => (
-                        <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700" key={user.username}>
-                            <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                        <tr className="bg-white border-b" key={user.username}>
+                            <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap">
                                 {user.username}
                             </th>
                             <td className="px-6 py-4 md-max:hidden">{user.password}</td>
--- anaglyph-letter-puzzle-game-frontend/src/components/Util/ObjectToTableView.tsx
+++ anaglyph-letter-puzzle-game-frontend/src/components/Util/ObjectToTableView.tsx
@@ -5,8 +5,8 @@ export default function PatientInfoToTableView({ obj }: { obj: Record<any, any> 
     const used = ["username", "password", "created", "lastUpdated"];
 
     return (
-        <table className="w-full text-sm text-left text-gray-500 dark:text-gray-400">
-            <thead className="text-xs text-gray-700 uppercase bg-gray-100 dark:bg-gray-700 dark:text-gray-400">
+        <table className="w-full text-sm text-left text-gray-500">
+            <thead className="text-xs text-gray-700 uppercase bg-gray-100">
                 <tr>
                     <th scope="col" className="px-6 py-3 rounded-l-lg">
                         Attribute
@@ -17,32 +17,32 @@ export default function PatientInfoToTableView({ obj }: { obj: Record<any, any> 
                 </tr>
             </thead>
             <tbody className="border-b-[1px]">
-                <tr className="bg-white dark:bg-gray-800">
-                    <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                <tr className="bg-white">
+                    <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap">
                         Username
                     </th>
                     <td className="px-6 py-4 whitespace-pre-wrap">{obj.username ?? "error"}</td>
                 </tr>
             </tbody>
             <tbody className="border-b-[1px]">
-                <tr className="bg-white dark:bg-gray-800">
-                    <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                <tr className="bg-white">
+                    <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap">
                         Password
                     </th>
                     <td className="px-6 py-4 whitespace-pre-wrap">{obj.password ?? "error"}</td>
                 </tr>
             </tbody>
             <tbody className="border-b-[1px]">
-                <tr className="bg-white dark:bg-gray-800">
-                    <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                <tr className="bg-white">
+                    <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap">
                         Date Created
                     </th>
                     <td className="px-6 py-4 whitespace-pre-wrap">{new Date(obj.created).toLocaleDateString()}</td>
                 </tr>
             </tbody>
             <tbody className="border-b-[1px]">
-                <tr className="bg-white dark:bg-gray-800">
-                    <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                <tr className="bg-white">
+                    <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap">
                         LastUpdated
                     </th>
                     <td className="px-6 py-4 whitespace-pre-wrap">{new Date(obj.lastUpdated * 1000).toLocaleDateString()}</td>
@@ -53,8 +53,8 @@ export default function PatientInfoToTableView({ obj }: { obj: Record<any, any> 
                 (key) =>
                     typeof obj[key] !== "object" && (
                         <tbody className="border-b-[1px]" key={key}>
-                            <tr className="bg-white dark:bg-gray-800">
-                                <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                            <tr className="bg-white">
+                                <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap">
                                     {key}
                                 </th>
                                 <td className="px-6 py-4 whitespace-pre-wrap">{obj[key]}</td>
