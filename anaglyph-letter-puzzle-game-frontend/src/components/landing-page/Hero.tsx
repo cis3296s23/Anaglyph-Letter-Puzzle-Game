1f2396428396440bc40f9ca9d069d00b90e256e2
Parth099 <parthnpatel715@gmail.com>
2023-04-25 13:50:27 -0400

    added 2 more banner images

--- anaglyph-letter-puzzle-game-frontend/src/components/landing-page/Hero.tsx
+++ anaglyph-letter-puzzle-game-frontend/src/components/landing-page/Hero.tsx
@@ -4,16 +4,14 @@ import { Gallery } from "react-grid-gallery";
 
 const images = [
     {
-        src: "https://c2.staticflickr.com/9/8817/28973449265_07e3aa5d2e_b.jpg",
+        src: "/hero-1.png",
         width: 320,
         height: 174,
-        caption: "After Rain (Jeshu John - designerspics.com)",
     },
     {
-        src: "https://c2.staticflickr.com/9/8356/28897120681_3b2c0f43e0_b.jpg",
+        src: "/hero-2.png",
         width: 320,
         height: 212,
-        alt: "Boats (Jeshu John - designerspics.com)",
     },
 
     {
